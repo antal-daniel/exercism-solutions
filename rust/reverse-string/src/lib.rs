83becba9706e4ba1957832dbcb07cc57b697ab8d
Dániel Antal <daniel.antal.0820@outlook.hu>
2024-08-02 17:26:02 +0200

    Solved exercise 'reverse-string' for Rust track

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,4 @@
 pub fn reverse(input: &str) -> String {
-    todo!("Write a function to reverse {input}");
+    // todo!("Write a function to reverse {input}");
+    input.chars().rev().collect()
 }
