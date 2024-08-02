af88b2acee3f762f37ac084a0907c76dc0d5d630
Dániel Antal <daniel.antal.0820@outlook.hu>
2024-08-02 17:52:43 +0200

    Added solution exercise 'gigasecond' for Rust track

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,9 @@
+use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    // todo!("What time is a gigasecond later than {start}");
+    const GIGASECOND: Duration = Duration::new(1_000_000_000, 0);
+    start.checked_add(GIGASECOND).unwrap()
 }
